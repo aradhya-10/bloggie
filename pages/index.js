60d36bbb95fb550bd8c87b8c9b48de8cdc7d4efb
aradhya-10 <202051034@iiitvadodara.ac.in>
2022-11-28 01:19:17 +0530

    Add files

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Image from 'next/image'
+// import styles from '../styles/globals.css'
+import Header from '../components/Header';
+import PostCard from '../components/PostCard';
+import PostWidget from '../components/PostWidget';
+import Categories from '../components/Categories';
+import { getPosts } from '../services'
+
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+		<Head>
+			<title>Bloggie</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+			<div className="lg:col-span-8 col-span-1">
+				{posts.map((post) => <PostCard post={post.node} key={post.title}/>)}
+			</div>
+		</div>
+		<div className="lg:col-span-4 col-span-1">
+			<div className="lg:sticky relative top-8">
+				<PostWidget/> 
+				<Categories/>
+			</div>
+		</div>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+	const posts = (await getPosts())||[]
+	return{
+		props: {posts}
+	}
+}
